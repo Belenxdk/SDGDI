a6f773a2f1695605ec17fd2127bea89fb5ed1ec7
Belenxdk <be.conalef@duocuc.cl>
2025-06-24 13:36:44 -0400

    Add files via upload
    belen conalef

---
+++ ConciertodeTrap.py
@@ -0,0 +1,26 @@
+
+
+
+while True :
+
+    print ("MENU PRINCIPAL")
+    print ("1.- Comprar entrada.")
+    print ("2.- Consultar comprador.")
+    print ("3.- Cancelar compra.")
+    print ("4.- Salir.")
+
+    if opcion == "1":
+        nomcomprador = input("Ingrese el nombre del comprador: ").lower()
+
+        tipoentrada = input("Ingrese tipo de entrada (G/V): ").lower()
+
+    elif opcion == "2":
+
+
+
+
+
+
+    
+
+
